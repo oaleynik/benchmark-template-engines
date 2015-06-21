2d43d92a3b05aa4d317e9b39206a78fd913f7791
Oleg Aleynik <oleg.aleynik@gmail.com>
2015-06-21 15:52:00 -0700

    feat(engine) add doT templating engine

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 var Mustache = require('mustache');
 var Hogan = require('Hogan');
+var Dot = require('dot');
 
 var Lodash = require('lodash');
 Lodash.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 
-var tpl = 'Hello, {{name}}!';
+///////////// Define template strings and context
+
+var templateStrings = {
+  Mustache: 'Hello, {{name}}!',
+  Hogan: 'Hello, {{name}}!',
+  Lodash: 'Hello, {{name}}!',
+  Dot: 'Hello, {{=it.name}}!'
+};
+
 var ctx = { name: 'Oleg' };
 
-Mustache.parse(tpl);
-var hoganTpl = Hogan.compile(tpl);
-var lodashTpl = Lodash.template(tpl);
+///////////// Precompile templates
 
-var suites = {
+Mustache.parse(templateStrings.Mustache);
+var hoganTpl = Hogan.compile(templateStrings.Hogan);
+var lodashTpl = Lodash.template(templateStrings.Lodash);
+var dotTpl = Dot.template(templateStrings.Dot);
+
+///////////// Define benchmark cases
+
+var benchmarks = {
   'Mustache#render': function() {
-    Mustache.render(tpl, ctx);
+    Mustache.render(templateStrings.Mustache, ctx);
   },
   'Hogan#render': function() {
     hoganTpl.render(ctx);
   },
   'Lodash#template': function() {
     lodashTpl(ctx);
+  },
+  'doT#template': function() {
+    dotTpl(ctx);
   }
 };
+
+///////////// Initialise suite
 
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
@@ -34,9 +53,13 @@ suite
     console.log('Fastest is ' + this.filter('fastest').pluck('name'));
   });
 
-Object.keys(suites).reduce(function(result, key) {
-  suite.add(key, suites[key]);
+///////////// Load benchmarks
+
+Object.keys(benchmarks).reduce(function(result, key) {
+  suite.add(key, benchmarks[key]);
 }, suite);
+
+///////////// Run
 
 suite.run({ 'async': true });
 
