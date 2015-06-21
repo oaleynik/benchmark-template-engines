024f5f2eb9793e536a2ce6d11efd623039617bba
Oleg Aleynik <oleg.aleynik@gmail.com>
2015-06-21 16:01:02 -0700

    feat(engine) add Handlebars templating engine

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Mustache = require('mustache');
 var Hogan = require('Hogan');
 var Dot = require('dot');
+var Handlebars = require('handlebars');
 
 var Lodash = require('lodash');
 Lodash.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
@@ -12,6 +13,7 @@ Underscore.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 
 var templateStrings = {
   Mustache: 'Hello, {{name}}!',
+  Handlebars: 'Hello, {{name}}!',
   Hogan: 'Hello, {{name}}!',
   Lodash: 'Hello, {{name}}!',
   Underscore: 'Hello, {{name}}!',
@@ -23,6 +25,7 @@ var ctx = { name: 'Oleg' };
 ///////////// Precompile templates
 
 Mustache.parse(templateStrings.Mustache);
+var handlebarsTpl = Handlebars.compile(templateStrings.Handlebars);
 var hoganTpl = Hogan.compile(templateStrings.Hogan);
 var lodashTpl = Lodash.template(templateStrings.Lodash);
 var underscoreTpl = Underscore.template(templateStrings.Underscore);
@@ -33,6 +36,9 @@ var dotTpl = Dot.template(templateStrings.Dot);
 var benchmarks = {
   'Mustache#render': function() {
     Mustache.render(templateStrings.Mustache, ctx);
+  },
+  'Handlebars#template': function() {
+    handlebarsTpl(ctx);
   },
   'Hogan#render': function() {
     hoganTpl.render(ctx);
